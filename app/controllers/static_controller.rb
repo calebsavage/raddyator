96277fb3565532bd02cd163306614c7078823b8f
Caleb Savage <calebsavagephoto@gmail.com>
2016-03-01 12:55:46 -0500

    Add humidity, timestamp to front page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,9 +4,9 @@ class StaticController < ApplicationController
 		@path = Config.find_by(:key => "path").value
 		@setpoint = Integer(Config.find_by(:key => "setpoint").value)
 		@range = Integer(Config.find_by(:key =>"range").value)
-		@current_inside = Integer(Datum.last.temp)
+		@current_inside = Datum.last
 
-		@diff = (@setpoint - @current_inside)
+		@diff = (@setpoint - Integer(@current_inside.temp))
 
 		if @diff > @range
 			@heating = true
