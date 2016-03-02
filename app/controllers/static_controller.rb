e1e412d8d950bbebe5bd2988396f5680f20e3344
Caleb Savage <calebsavagephoto@gmail.com>
2016-03-02 15:01:03 -0500

    Limit data on graph to 20 data points

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -8,7 +8,7 @@ class StaticController < ApplicationController
 
 		@diff = (@setpoint - Integer(@current_inside.temp))
 
-		@chart_data = Datum.all
+		@chart_data = Datum.limit(20)
 
 		if @diff > @range
 			@heating = true
